a6946ed7a09429828f4e3a3b8197c00d2c1fe982
Jaderson Jonas Silva <Jader_19_02@hotmail.com>
2019-10-14 23:26:41 -0300

    CalculadoraQuasePronta

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
-function addValor(valor) {
+function insereValorClicado(valor) {
 	document.getElementById("EntradaDados").value += valor
 }
 
-function Calcular() {
+function calcular() {
 	var valor = document.getElementById("EntradaDados").value
+	console.log(valor)
+	if (valor == "")
+	{
+		console.log(valor)
+	} else {
 	document.getElementById("EntradaDados").value = eval(valor)
+	}
 }
 
-function Limpar() {
+function limpar() {
     document.getElementById("EntradaDados").value = ''
 }
+
+document.onkeypress = function verificaTeclaPressionada(){
+	var teclaEmUnicode = window.event.keyCode
+	var teclaConvertidaChar = String.fromCharCode(teclaEmUnicode)
+	if(teclaEmUnicode >= 48 && teclaEmUnicode <= 57 
+		|| teclaEmUnicode == 42 
+		|| teclaEmUnicode == 43 
+		|| teclaEmUnicode == 45 
+		|| teclaEmUnicode == 47)
+	{
+		document.getElementById("EntradaDados").value += teclaConvertidaChar
+	}
+	else if(teclaEmUnicode == 13)
+	{
+		calcular()
+	}
+}
+
