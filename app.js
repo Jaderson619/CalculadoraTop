4fb2d085d79282fb82de3afdd45fe95094ee2374
Jaderson Jonas Silva <Jader_19_02@hotmail.com>
2019-10-16 10:30:41 -0300

    CleanCodeBrabo

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 function inserirValorNaTela(valor) {
-	document.getElementById("EntradaDados").value += valor
+	document.getElementById("EntradaNumerosNaTelaCalculadora").value += valor
 }
 
 function calcular() {
-	var valor = document.getElementById("EntradaDados").value
+	var valor = document.getElementById("EntradaNumerosNaTelaCalculadora").value
 
 	if (valor == "")
 	{
 	} else {
-	document.getElementById("EntradaDados").value = eval(valor)
+	document.getElementById("EntradaNumerosNaTelaCalculadora").value = eval(valor)
 	}
 }
 
 function limpar() {
-    document.getElementById("EntradaDados").value = ''
+    document.getElementById("EntradaNumerosNaTelaCalculadora").value = ''
 }
 
 document.onkeypress = function validarTeclaPressionada(){
-	var teclaEmDecimal = event.keyCode
-	var teclaConvertidaChar = String.fromCharCode(teclaEmDecimal)
-	if(teclaEmDecimal >= 48 && teclaEmDecimal <= 57 
-		|| teclaEmDecimal == 42 && ultimoCaractereDoValor != "*"
-		|| teclaEmDecimal == 43 && ultimoCaractereDoValor != "+" 
-		|| teclaEmDecimal == 45 && ultimoCaractereDoValor != "-"
-		|| teclaEmDecimal == 47 && ultimoCaractereDoValor != "/")
+	var teclaEmASCII = event.keyCode
+	var teclaConvertidaChar = String.fromCharCode(teclaEmASCII)
+	if(teclaEmASCII >= 48 && teclaEmASCII <= 57 
+		|| teclaEmASCII == 42 && ultimoCaractereDoValor != "*"
+		|| teclaEmASCII == 43 && ultimoCaractereDoValor != "+" 
+		|| teclaEmASCII == 45 && ultimoCaractereDoValor != "-"
+		|| teclaEmASCII == 47 && ultimoCaractereDoValor != "/")
 	{
 		inserirValorNaTela(teclaConvertidaChar)
 		ultimoCaractereDoValor = teclaConvertidaChar
 		console.log(ultimoCaractereDoValor)
 	}
-	else if(teclaEmDecimal == 13)
+	else if(teclaEmASCII == 13)
 	{
 		calcular()
 	}
